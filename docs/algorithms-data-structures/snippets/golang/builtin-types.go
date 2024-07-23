c3437ad9bbdacb5e30bfd225310804454978929c
Igor Lima <lima.igorribeiro@gmail.com>
2024-07-23 14:53:29 -0300

    enhancement in go snippets related to built-in types

--- docs/algorithms-data-structures/snippets/golang/builtin-types.go
+++ docs/algorithms-data-structures/snippets/golang/builtin-types.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	/*
+		ZERO VALUES
+	*/
 	var status bool
 	var MyString string
 	var Id uint64
@@ -15,6 +18,9 @@ func main() {
 	fmt.Printf("Type of real is %T and has value %v\n", real, real)
 	fmt.Println("...............................\n")
 
+	/*
+	  type declarations
+	*/
 	var boolean bool = true
 	var Text string = "Hello, World!"
 	var U8 uint8 = 255
@@ -37,9 +43,43 @@ func main() {
 	fmt.Printf("Type of U8B is %T and has value %v\n", U8B, U8B)
 	fmt.Println("...............................\n")
 
+	/*
+		In the following declarations, the types of the multiple variables
+		declared in the same declaration line can be different.
+	*/
+	// The types of the lang and dynamic variables
+	// will be deduced as built-in types "string"
+	// and "bool" by compilers, respectively.
+	var lang, dynamic = "Go", false
+	// The types of the compiled and announceYear
+	// variables will be deduced as built-in
+	// types "bool" and "int", respectively.
+	var compiled, announceYear = true, 2009
+	_, _, _, _ = lang, dynamic, compiled, announceYear // make lang, dynamic, compiled, and announceYear used.
+
+	/*
+		Multiple variables can be grouped into
+		one standard form declaration by using ().
+	*/
+	var (
+		announceAt, releaseAt int = 2009, 2012
+		createdBy, website    string
+	)
+	_, _, _, _ = announceAt, releaseAt, createdBy, website // make announceAt, releaseAt, createdBy, and website used.
+
+	/*
+		SHORT VARIABLE DECLARATION FORMS
+	*/
+	/*
+		Deductions in Go
+		Go compilers will deduce the types for these values by context.
+		Type deduction is also often called type inference.
+	*/
+	// The types of variables booleanC and TextC will be
+	// deduced as the built-in type "bool" and "string",
 	booleanC := true
 	TextC := "Hello, World!"
-	fmt.Println("Declaring Variables in Go")
+	fmt.Println("Deduced Type Variables in Go")
 	fmt.Println("-------------------------------")
 	fmt.Printf("Type of booleanC is %T and has value %v\n", booleanC, booleanC)
 	fmt.Printf("Type of TextC is %T and has value %v\n", TextC, TextC)
@@ -51,10 +91,35 @@ func main() {
 	   the following two string literals are equivalent:
 	*/
 	fmt.Println("Declaring String Literals in Go")
+	fmt.Println("-------------------------------")
 	var interpretedString = "Hello\nworld!\n\"你好世界\""
 	var rawString = `Hello
 	world!
 	"你好世界"`
 	fmt.Println("The interpreted string is: ", interpretedString)
 	fmt.Println("The raw string is: ", rawString)
+	fmt.Println("...............................\n")
+
+	fmt.Println("Constants in Go")
+	fmt.Println("-------------------------------")
+	// Declare two individual constants. Yes,
+	// non-ASCII letters can be used in identifiers.
+	const π = 3.1416
+	const Pi = π // <=> const Pi = 3.1416
+	// Declare multiple constants in a group.
+	const (
+		Yes        = true
+		No         = !Yes
+		MaxDegrees = 360
+		Unit       = "radian"
+	)
+	// typed named constants
+	const X float32 = 3.14
+	const (
+		A, B int64   = -3, 5
+		Y    float32 = 2.718
+	)
+	fmt.Println("The value of π is: ", π)
+	fmt.Println("...............................\n")
+
 }
