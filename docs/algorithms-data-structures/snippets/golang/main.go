6e8d987991bca6faffcf2e23f84bc9107036a88b
Igor Lima <lima.igorribeiro@gmail.com>
2024-07-23 11:18:03 -0300

    draft golang snippets

---
+++ docs/algorithms-data-structures/snippets/golang/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/*
+ * Makefile for Golang:
+ *  https://igorlima.github.io/unapologetic-snippets/docs/languages/golang/other-makefile
+ * Download Makefile:
+ *  `https://makefile-go-gist.ilima.xyz/`
+ *  `curl -L https://makefile-go-download.ilima.xyz -o Makefile`
+ */
+
+func main() {
+	fmt.Println("vim-go")
+}
