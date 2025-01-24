8766c2022330889a64dc7063d16270328e3b6849
Igor Lima <lima.igorribeiro@gmail.com>
2025-01-24 13:32:21 -0300

    create bundle folder to simplify a few playground samples

---
+++ docs/languages/js/bundle/react-aria-component-20250124-13-21/index.js
@@ -0,0 +1,11 @@
+import { Button, Form, Input, Label, TextField } from "react-aria-components";
+
+import {
+  ListBox,
+  ListBoxItem,
+  Popover,
+  Select,
+  SelectValue,
+} from "react-aria-components";
+
+export { Button, Form, Input, Label, TextField, ListBox, ListBoxItem, Popover, Select, SelectValue };
