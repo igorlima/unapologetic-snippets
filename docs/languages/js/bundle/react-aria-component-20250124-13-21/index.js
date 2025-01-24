ffa1c79468b6282d06ed8f3b35a076cfe859a90b
Igor Lima <lima.igorribeiro@gmail.com>
2025-01-24 14:12:30 -0300

    add React to the bundle

--- docs/languages/js/bundle/react-aria-component-20250124-13-21/index.js
+++ docs/languages/js/bundle/react-aria-component-20250124-13-21/index.js
@@ -1,4 +1,6 @@
 import { Button, Form, Input, Label, TextField } from "react-aria-components";
+import React from "react";
+import ReactDOM from "react-dom";
 
 import {
   ListBox,
@@ -8,4 +10,4 @@ import {
   SelectValue,
 } from "react-aria-components";
 
-export { Button, Form, Input, Label, TextField, ListBox, ListBoxItem, Popover, Select, SelectValue };
+export { Button, Form, Input, Label, TextField, ListBox, ListBoxItem, Popover, Select, SelectValue, React, ReactDOM };
